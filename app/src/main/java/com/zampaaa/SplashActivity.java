780f5561d2a1c0d39ed6a43cd387c424f58e5ed2
kirankumarkyasa <db2kyasa@gmail.com>
2016-05-28 20:25:29 +0530

    splash and digits

--- app/src/main/java/com/zampaaa/SplashActivity.java
+++ app/src/main/java/com/zampaaa/SplashActivity.java
@@ -1,13 +1,23 @@
 package com.zampaaa;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },4000);
+
     }
 }
