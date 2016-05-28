4349013b99bfa72bea3d8c529c430d00e812bea4
anagarjuna <nagarjunaalaparthi@gmail.com>
2016-05-28 22:40:38 +0530

    started report

---
+++ app/src/main/java/com/zampaaa/Analytics/ReportsActivity.java
@@ -0,0 +1,15 @@
+package com.zampaaa.Analytics;
+
+import android.os.Bundle;
+
+import com.zampaaa.BaseActivity;
+
+/**
+ * Created by Softapt on 28/05/2016.
+ */
+public class ReportsActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
