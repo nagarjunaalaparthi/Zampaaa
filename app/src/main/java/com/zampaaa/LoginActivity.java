0818b6973aae390ef8dd02d846b08293f190d3b5
kiran.kyasa <kiran.kyasa@xcubelabs.com>
2016-05-29 00:32:50 +0530

    login Activity added

--- app/src/main/java/com/zampaaa/LoginActivity.java
+++ app/src/main/java/com/zampaaa/LoginActivity.java
@@ -1,13 +1,57 @@
 package com.zampaaa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.Digits;
+import com.digits.sdk.android.DigitsAuthConfig;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button mVerify;
+    private EditText mPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mPhoneNumber = (EditText) findViewById(R.id.phone_number);
+        mVerify = (Button) findViewById(R.id.verify);
+        mVerify.setOnClickListener(verifyClickListener);
     }
+
+    View.OnClickListener verifyClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (mPhoneNumber.getText().toString().length() > 0) {
+                DigitsAuthConfig config = new DigitsAuthConfig.Builder().withAuthCallBack(authCallback).withPhoneNumber("+91" + mPhoneNumber.getText().toString()).build();
+                Digits.getSessionManager().clearActiveSession();
+                Digits.authenticate(config);
+            }
+        }
+    };
+
+    AuthCallback authCallback = new AuthCallback() {
+        @Override
+        public void success(DigitsSession session, String phoneNumber) {
+            // TODO: associate the session userID with your user model
+            Intent restaurentIntent = new Intent(LoginActivity.this, RestaurentActivity.class);
+            startActivity(restaurentIntent);
+            SharedPreferenceUtils.writeBoolean(LoginActivity.this,"login",true);
+            finish();
+        }
+
+        @Override
+        public void failure(DigitsException exception) {
+            Log.d("Digits", "Sign in with Digits failure", exception);
+        }
+    };
 }
